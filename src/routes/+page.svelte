a043d7c9b7c3c2c6e5fd1aed6123ba40404c6ef4
cheychus <cichacki@ipk-gatersleben.de>
2026-02-10 14:21:12 +0100

    Implemented JSON Import for ARCs and added some UI Components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,32 @@
-<h1 class="font-bold text-5xl w-full flex justify-center bg-amber-50 py-4">Ontology Harmonizer</h1>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+
+  let fileInput: HTMLInputElement;
+
+  function handleChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files) return;
+    const file = input.files[0];
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      try {
+        const json = JSON.parse(reader.result as string);
+        console.log(json);
+      } catch (e) {
+        alert("Ungültige JSON-Datei");
+      }
+    };
+    console.log(file);
+
+    reader.readAsText(file);
+  }
+
+  const allowedFormats = "application/json,.json";
+</script>
+
+<div class="min-h-screen flex justify-center items-center">
+  <Button class="p-6" onclick={() => fileInput.click()}>Upload ARC-RO-Crate JSON File</Button>
+  <input class="hidden" type="file" accept={allowedFormats} bind:this={fileInput} onchange={handleChange} />
+</div>
