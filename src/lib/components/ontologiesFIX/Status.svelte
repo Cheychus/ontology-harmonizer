13de61a745a5d5f540c9e3f8e61ed85cf098bac5
cheychus <cichacki@ipk-gatersleben.de>
2026-02-25 13:43:27 +0100

    added base for goto()

--- src/lib/components/ontologiesFIX/Status.svelte
+++ src/lib/components/ontologiesFIX/Status.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-    import { arcStore } from "$lib/stores/ArcStore.svelte";
-    import * as Table from "$lib/components/ui/table/index.js";
-    import { terminologyStore } from "$lib/stores/terminologyService/TerminologyStore.svelte";
-    import Button from "../ui/button/button.svelte";
-    import { goto, preloadData } from "$app/navigation";
-    import Collection from "../terminology/Collection.svelte";
-    import { Settings, X } from "lucide-svelte";
+  import { arcStore } from "$lib/stores/ArcStore.svelte";
+  import * as Table from "$lib/components/ui/table/index.js";
+  import { terminologyStore } from "$lib/stores/terminologyService/TerminologyStore.svelte";
+  import Button from "../ui/button/button.svelte";
+  import { goto, preloadData } from "$app/navigation";
+  import Collection from "../terminology/Collection.svelte";
+  import { Settings, X } from "lucide-svelte";
+  import { base } from "$service-worker";
 
-    let statusLog: string[] = $state([]);
+  let statusLog: string[] = $state([]);
 </script>
 
 <div class="flex w-full flex-col gap-2 py-4">
-    <h2 class="underline">Status Information</h2>
-    <p>Filename: {arcStore.filename}</p>
-    <!-- <Table.Root>
+  <h2 class="underline">Status Information</h2>
+  <p>Filename: {arcStore.filename}</p>
+  <!-- <Table.Root>
         <Table.Caption></Table.Caption>
         <Table.Header>
             <Table.Row>
@@ -33,37 +34,37 @@
         </Table.Body>
     </Table.Root> -->
 
-    <div class="flex gap-4 items-center" role="status" onmouseenter={() => preloadData("/collections")}>
-        <h3>Collection</h3>
-        <Button
-            variant={"outline"}
-            size={"icon"}
-            onclick={() => {
-                goto("/collections");
-            }}><Settings class="w-6! h-6!" /></Button
-        >
-
-        {#if terminologyStore.selectedCollection}
-            <Button
-                class="ml-auto"
-                size="icon-sm"
-                variant={"outline"}
-                onclick={() => {
-                    terminologyStore.removeSelection();
-                }}
-                ><X />
-            </Button>
-        {/if}
-    </div>
+  <div class="flex gap-4 items-center" role="status" onmouseenter={() => preloadData("/collections")}>
+    <h3>Collection</h3>
+    <Button
+      variant={"outline"}
+      size={"icon"}
+      onclick={() => {
+        goto(`${base}/collections`);
+      }}><Settings class="w-6! h-6!" /></Button
+    >
 
     {#if terminologyStore.selectedCollection}
-        <Collection collection={terminologyStore.selectedCollection} selectButton={false} />
-    {:else}
-        <p>Define a collection to reduce the amounts of ontology search results</p>
+      <Button
+        class="ml-auto"
+        size="icon-sm"
+        variant={"outline"}
+        onclick={() => {
+          terminologyStore.removeSelection();
+        }}
+        ><X />
+      </Button>
     {/if}
+  </div>
 
-    <h4 class="underline">Status Messages</h4>
-    {#each statusLog as log}
-        <p class="text-green-400">{log}</p>
-    {/each}
+  {#if terminologyStore.selectedCollection}
+    <Collection collection={terminologyStore.selectedCollection} selectButton={false} />
+  {:else}
+    <p>Define a collection to reduce the amounts of ontology search results</p>
+  {/if}
+
+  <h4 class="underline">Status Messages</h4>
+  {#each statusLog as log}
+    <p class="text-green-400">{log}</p>
+  {/each}
 </div>
