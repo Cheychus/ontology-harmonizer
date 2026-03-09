0c4a34a773773a2ed9f32ce87787d57f4ab12b08
Alexander Cichacki <a.cichacki@ostfalia.de>
2026-03-09 12:41:12 +0100

    added vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,14 +7,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: '/ontology-harmonizer'
-		}
+		adapter: vercel(),
+		paths: { base: '' } // optional
 	}
 };
 
